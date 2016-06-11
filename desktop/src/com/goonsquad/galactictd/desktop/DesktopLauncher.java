15ca1cec942c09dd7714d38a36da9262955cd738
Andrew <amverhagen93@gmail.com>
2016-06-11 19:48:53 -0400

    Start converting game to use new libgdx model.
    Do this so that newest version of LibGDX can be used, so that gradle can be used, so that repo has proper structure.

---
+++ desktop/src/com/goonsquad/galactictd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.goonsquad.galactictd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.goonsquad.galactictd.GalacticTDGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GalacticTDGame(), config);
+	}
+}
