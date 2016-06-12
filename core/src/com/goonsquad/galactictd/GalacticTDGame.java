dde0d9d6ab1b1c1ec24d6f885e73803e305231af
Andrew <amverhagen93@gmail.com>
2016-06-11 20:36:21 -0400

    Move assets to gradle game version.
    Do this so that gradle game can use assets.

--- core/src/com/goonsquad/galactictd/GalacticTDGame.java
+++ core/src/com/goonsquad/galactictd/GalacticTDGame.java
@@ -13,7 +13,7 @@ public class GalacticTDGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Owens_Frank.jpg");
 	}
 
 	@Override
