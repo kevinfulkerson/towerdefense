eeadec3975b83fe6024436dd895e2c414e3789f9
Andrew <amverhagen93@gmail.com>
2016-06-18 23:09:03 -0400

    Make ships fly across screen and come back to original spot.

--- core/src/com/goonsquad/galactictd/screens/ScoreScreen.java
+++ core/src/com/goonsquad/galactictd/screens/ScoreScreen.java
@@ -4,16 +4,12 @@ package com.goonsquad.galactictd.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.goonsquad.galactictd.GalacticTDGame;
-
-import java.util.ArrayList;
 
 public class ScoreScreen implements Screen, InputProcessor {
     private static final String TAG = "ScoreScreen";
@@ -23,18 +19,18 @@ public class ScoreScreen implements Screen, InputProcessor {
     private Sprite background;
     private Sprite greenShip;
     private Sprite redShip;
-    private FileHandle scoresDataFile;
     private String scoresAsString;
-    private ArrayList<Integer> highScores = new ArrayList<Integer>();
     private String[] stringArray;
     private BitmapFont text;
     private boolean loaded;
+    private float shipSpeedPerSecond;
 
     public ScoreScreen(GalacticTDGame game) {
         Gdx.app.log(TAG, "Initialized " + TAG);
         this.gameInstance = game;
         batch = new SpriteBatch();
         loaded = false;
+        shipSpeedPerSecond = 400f;
     }
 
     private void loadScreenObjects() {
@@ -42,15 +38,17 @@ public class ScoreScreen implements Screen, InputProcessor {
         background.setSize(GalacticTDGame.UI_WIDTH, GalacticTDGame.UI_HEIGHT);
 
         Vector2 shipSize = new Vector2(GalacticTDGame.UI_WIDTH / 16, GalacticTDGame.UI_WIDTH / 16f);
+
         greenShip = new Sprite(gameInstance.getAssetManager().get("tower-green.png", Texture.class));
         greenShip.setSize(shipSize.x, shipSize.y);
         greenShip.setOriginCenter();
-        greenShip.setPosition(GalacticTDGame.UI_WIDTH - greenShip.getWidth(), GalacticTDGame.UI_HEIGHT - greenShip.getHeight());
+        greenShip.setPosition(GalacticTDGame.UI_WIDTH, GalacticTDGame.UI_HEIGHT - shipSize.y);
         greenShip.rotate(90);
 
         redShip = new Sprite(gameInstance.getAssetManager().get("tower-red.png", Texture.class));
         redShip.setSize(shipSize.x, shipSize.y);
         redShip.setOriginCenter();
+        redShip.setPosition(0 - shipSize.x, 0);
         redShip.rotate(-90);
 
         loaded = true;
@@ -58,15 +56,16 @@ public class ScoreScreen implements Screen, InputProcessor {
 
     @Override
     public void render(float delta) {
-//        greenShip.translateX(-9);
-//        redShip.translateX(7);
+        greenShip.translateX(shipSpeedPerSecond * delta * -1);
+        redShip.translateX(shipSpeedPerSecond * delta);
 
-//        if (greenShip.getX() < -width / 10) {
-//            greenShip.setPosition(width, (height - (width / 6)));
-//        }
-//        if (redShip.getX() > width) {
-//            redShip.setPosition(-width / 10, height / 6);
-//        }
+        if (greenShip.getX() < (0 - greenShip.getWidth() * 2)) {
+            greenShip.setX(GalacticTDGame.UI_WIDTH);
+        }
+
+        if (redShip.getX() > GalacticTDGame.UI_WIDTH + redShip.getWidth()) {
+            redShip.setX(0 - (redShip.getWidth()));
+        }
         batch.setProjectionMatrix(gameInstance.getUiCamera().combined);
         batch.begin();
 
@@ -90,68 +89,9 @@ public class ScoreScreen implements Screen, InputProcessor {
         Gdx.input.setInputProcessor(this);
         if (!loaded) loadScreenObjects();
 
-//        getScoresFile();
 //        text = new BitmapFont();
     }
 
-    public void sendScore(int score) {
-        getScoresFile();
-        sortScores();
-        int newScore = newHighScore(score);
-        if (newScore != 5) {
-            highScores.add(newScore, score);
-            highScores.remove(5);
-        }
-        scoresDataFile.writeString(highScores.get(0) + " " + highScores.get(1) + " " + highScores.get(2) + " "
-                + highScores.get(3) + " " + highScores.get(4), false);
-    }
-
-    private int newHighScore(int score) {
-        for (int i : highScores) {
-            if (score > i) {
-                return highScores.indexOf(i);
-            }
-        }
-        return 5;
-    }
-
-    public void getScoresFile() {
-        try {
-            scoresDataFile = Gdx.files.local("highScores.txt");
-            scoresAsString = scoresDataFile.readString();
-        } catch (GdxRuntimeException e) {
-            scoresDataFile = Gdx.files.local("highScores.txt");
-            scoresDataFile.writeString("0 0 0 0 0", false);
-            scoresDataFile = Gdx.files.local("highScores.txt");
-            scoresAsString = scoresDataFile.readString();
-        }
-        stringArray = scoresAsString.split(" ");
-        for (int i = 0; i < 5; i++) {
-            highScores.add(Integer.parseInt(stringArray[i]));
-        }
-        sortScores();
-    }
-
-    public void sortScores() {
-        int[] temp = new int[5];
-        for (int i = 0; i < 5; i++) {
-            temp[i] = highScores.remove(0);
-        }
-
-        for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < 5; k++) {
-                if (temp[j] > temp[k]) {
-                    int holder = temp[j];
-                    temp[j] = temp[k];
-                    temp[k] = holder;
-                }
-            }
-        }
-
-        for (int s : temp) {
-            highScores.add(s);
-        }
-    }
 
     @Override
     public void resize(int width, int height) {
@@ -171,7 +111,6 @@ public class ScoreScreen implements Screen, InputProcessor {
     @Override
     public void resume() {
         Gdx.app.log(TAG, "resume() called.");
-//        scoresDataFile = null;
     }
 
     @Override
