d453bbf5a7b9b2c2d59f8016936437eeb0ef6ed5
Andrew <amverhagen93@gmail.com>
2016-06-29 16:57:45 -0400

    Remove settings objects from home screen.

--- core/src/com/goonsquad/galactictd/screens/HomeScreen.java
+++ core/src/com/goonsquad/galactictd/screens/HomeScreen.java
@@ -3,14 +3,7 @@ package com.goonsquad.galactictd.screens;
 import com.artemis.World;
 import com.artemis.WorldConfiguration;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.goonsquad.galactictd.GalacticTDGame;
 import com.goonsquad.galactictd.systems.archtypes.HomeScreenArchetypeBuilder;
 import com.goonsquad.galactictd.systems.graphics.BoxRenderSystem;
@@ -18,29 +11,15 @@ import com.goonsquad.galactictd.systems.graphics.UiRenderSystem;
 import com.goonsquad.galactictd.systems.initialization.HomeScreenInitSystem;
 import com.goonsquad.galactictd.systems.input.UiTouchSystem;
 
-public class HomeScreen implements Screen, InputProcessor {
+public class HomeScreen implements Screen {
     private static final String TAG = "HomeScreen";
     private World homeScreenWorld;
     private GalacticTDGame gameInstance;
-    private SpriteBatch batch;
-    private Sprite quitButton;
-    private Sprite settingsBackdrop;
-    private Sprite settingsMenu;
-    private Vector2 settingsMenuEnd;
-    private Vector2 settingsMenuStart;
-    private final float settingsMenuSpeed = 1f;
-    private float settingsMenuElapsedTime = 0f;
-    private Sprite soundButton;
-    private boolean settingsOn;
-    private boolean loaded;
     private UiTouchSystem uiTouchSystem;
 
     public HomeScreen(GalacticTDGame game) {
         Gdx.app.log(TAG, "Initialized " + TAG);
         this.gameInstance = game;
-        batch = new SpriteBatch();
-        this.settingsOn = false;
-        this.loaded = false;
     }
 
     //A WorldConfig is used to build a world so that dependency injection can occur.
@@ -71,113 +50,16 @@ public class HomeScreen implements Screen, InputProcessor {
         Gdx.input.setInputProcessor(uiTouchSystem);
     }
 
-    public void loadScreenObjects() {
-        createHomeScreenObjects();
-        setHomeScreenSpriteBounds(GalacticTDGame.UI_WIDTH, GalacticTDGame.UI_HEIGHT);
-        createSettingsObjects();
-        setSettingsSpriteBounds(GalacticTDGame.UI_WIDTH, GalacticTDGame.UI_HEIGHT);
-        this.loaded = true;
-    }
-
-    private void createHomeScreenObjects() {
-        quitButton = new Sprite(gameInstance.getAssetManager().get("buttonQuit.png", Texture.class));
-
-    }
-
-    private void setHomeScreenSpriteBounds(float width, float height) {
-        Vector2 buttonSize = new Vector2(300f, 150f);
-
-    }
-
-    private void createSettingsObjects() {
-        settingsBackdrop = new Sprite(gameInstance.getAssetManager().get("black.png", Texture.class));
-        settingsBackdrop.setAlpha(.8f);
-        settingsMenu = new Sprite(gameInstance.getAssetManager().get("border.png", Texture.class));
-        soundButton = new Sprite(gameInstance.getAssetManager().get("music_icon.png", Texture.class));
-        soundButton.setColor(255, 255, 255, 255);
-    }
-
-    private void setSettingsSpriteBounds(float width, float height) {
-        settingsBackdrop.setSize(width, height);
-
-        settingsMenu.setSize(width / 4f, width / 4f);
-        settingsMenuEnd = new Vector2(width - settingsMenu.getWidth(), 0);
-        settingsMenuStart = new Vector2(width, -settingsMenu.getHeight());
-        settingsMenu.setPosition(settingsMenuStart.x, settingsMenuStart.y);
-
-        soundButton.setSize(width / 16f, width / 16f);
-        soundButton.setPosition(settingsMenuEnd.x + soundButton.getWidth(), settingsMenu.getHeight() - soundButton.getHeight());
-    }
-
     @Override
     public void render(float delta) {
-//        batch.setProjectionMatrix(gameInstance.getUiCamera().combined);
-//        batch.begin();
-//        renderHome(batch);
-//        if (settingsOn) {
-//            updateSettings(delta);
-//            renderSettings(batch);
-//        }
-//        batch.end();
         homeScreenWorld.setDelta(delta);
         homeScreenWorld.process();
-    }
-
-    private void renderHome(SpriteBatch batch) {
-        quitButton.draw(batch);
-    }
-
-    private void updateSettings(float delta) {
-        if (settingsMenuElapsedTime < settingsMenuSpeed) {
-            settingsMenuElapsedTime += delta;
-            float movementAmount = MathUtils.clamp(settingsMenuElapsedTime, 0, settingsMenuSpeed);
-
-            float newX = MathUtils.lerp(settingsMenu.getX(), settingsMenuEnd.x, movementAmount);
-            float newY = MathUtils.lerp(settingsMenu.getY(), settingsMenuEnd.y, movementAmount);
-
-            settingsMenu.setPosition(newX, newY);
-        }
-    }
-
-    private void renderSettings(SpriteBatch batch) {
-        settingsBackdrop.draw(batch);
-        settingsMenu.draw(batch);
-        soundButton.draw(batch);
-    }
-
-    private boolean handleSettingsTouch(Vector3 touchLocation) {
-        return toggleSettings();
-    }
-
-    private boolean toggleSettings() {
-        settingsMenu.setPosition(settingsMenuStart.x, settingsMenuStart.y);
-        settingsMenuElapsedTime = 0f;
-        settingsOn = !settingsOn;
-        return true;
-    }
-
-    private boolean handleHomeTouch(Vector3 touchLocation) {
-//        if (playButton.getBoundingRectangle().contains(touchLocation.x, touchLocation.y)) {
-//            return true;
-//        } else if (quitButton.getBoundingRectangle().contains(touchLocation.x, touchLocation.y)) {
-//            Gdx.app.exit();
-//            return true;
-//        }
-//        else if (scoresButton.getBoundingRectangle().contains(touchLocation.x, touchLocation.y)) {
-//            gameInstance.getScreenManager().setScreen(ScoreScreen.class);
-//            return true;
-//        }
-//        else if (settingsButton.getBoundingRectangle().contains(touchLocation.x, touchLocation.y)) {
-//            return toggleSettings();
-//        }
-        return false;
     }
 
     @Override
     public void resize(int width, int height) {
         Gdx.app.log(TAG, "resize() called.");
     }
-
 
     @Override
     public void hide() {
@@ -195,58 +77,8 @@ public class HomeScreen implements Screen, InputProcessor {
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        Vector3 touchLocation = new Vector3(screenX, screenY, 0);
-        touchLocation = gameInstance.getUiViewport().unproject(touchLocation);
-        if (settingsOn) {
-            return handleSettingsTouch(touchLocation);
-        } else {
-            return handleHomeTouch(touchLocation);
-        }
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
-    }
-
-    @Override
     public void dispose() {
         Gdx.app.log(TAG, "dispose() called.");
-        batch.dispose();
-        if (loaded) {
-            quitButton.getTexture().dispose();
-        }
     }
 }
 
