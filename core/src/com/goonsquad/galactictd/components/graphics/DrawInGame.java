8e3dc684f767a6eefb3ff86be125a24403d0cedb
Andrew <amverhagen93@gmail.com>
2016-06-29 17:04:38 -0400

    Change DrawInUi and DrawInGame to poolable components.

--- core/src/com/goonsquad/galactictd/components/graphics/DrawInGame.java
+++ core/src/com/goonsquad/galactictd/components/graphics/DrawInGame.java
@@ -2,5 +2,9 @@ package com.goonsquad.galactictd.components.graphics;
 
 import com.artemis.Component;
 
-public class DrawInGame extends Component {
+public class DrawInGame extends com.artemis.PooledComponent {
+    @Override
+    protected void reset() {
+
+    }
 }
--- core/src/com/goonsquad/galactictd/components/graphics/DrawInUi.java
+++ core/src/com/goonsquad/galactictd/components/graphics/DrawInUi.java
@@ -2,5 +2,9 @@ package com.goonsquad.galactictd.components.graphics;
 
 import com.artemis.Component;
 
-public class DrawInUi extends Component {
+public class DrawInUi extends com.artemis.PooledComponent {
+    @Override
+    protected void reset() {
+        
+    }
 }
